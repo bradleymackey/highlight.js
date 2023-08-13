7bf24121740e5d55f1e9d649915c2e273b8674d6
Bradley Mackey <bradleymackey@me.com>
2023-08-13 09:31:12 +0100

    Use custom build script

---
+++ tools/buildSizeReport.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+/*
+ * Input: path to 2 build directories to compare.
+ * Output: markdown report of size changes.
+ */
+
+const fs = require("fs");
+const path = require("path");
+const zlib = require("zlib");
+
+/**
+ * The size, in bytes of the given file after gzip.
+ */
+function computedFile(dir, filePath) {
+  const pathToFile = path.join(dir, filePath);
+  const str = fs.readFileSync(pathToFile);
+  return zlib.gzipSync(str).length;
+}
+
+/**
+ * Returns mardown report of size differences.
+ */
+function run() {
+  const [base, pr] = process.argv.slice(2);
+  const files = ["highlight.min.js", "es/highlight.min.js"];
+
+  let md = "# CDN Size Report\n";
+  md += "| file | base | pr | diff |\n";
+  md += "| --- | --- | --- | --- |\n";
+  for (const file of files) {
+    const computedBase = computedFile(base, file);
+    const computedPR = computedFile(pr, file);
+    const diff = computedPR - computedBase;
+    const sign = diff >= 0 ? "+" : "-";
+    md += `| ${file} | ${computedBase}B | ${computedPR}B | ${sign}${diff}B\n`;
+  }
+
+  return md;
+}
+
+console.log(run());
