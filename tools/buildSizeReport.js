6f8f085747232813e7ba823c5ab6c276aeb49764
Bradley Mackey <bradleymackey@me.com>
2023-08-13 11:15:11 +0100

    Disclose changes in details

--- tools/buildSizeReport.js
+++ tools/buildSizeReport.js
@@ -67,27 +67,32 @@ async function run() {
   if (addedFiles.length > 0) {
     const maybeS = addedFiles.length === 1 ? "" : "s";
     md += `## ${addedFiles.length} Added File${maybeS}\n\n`;
+    md += "<details>\n";
     md += "| file | size |\n";
     for (const file of addedFiles) {
       const computedSize = computedFile(pr, file);
       md += `| ${file} | +${computedSize}B |\n`;
     }
+    md += "</details>\n";
     md += "\n";
   }
 
   if (removedFiles.length > 0) {
     const maybeS = removedFiles.length === 1 ? "" : "s";
     md += `## ${removedFiles.length} Removed File${maybeS}\n\n`;
+    md += "<details>\n";
     md += "| file | size |\n";
     for (const file of removedFiles) {
       const computedSize = computedFile(base, file);
       md += `| ${file} | -${computedSize}B |\n`;
     }
+    md += "</details>\n";
     md += "\n";
   }
 
   let fileSizeChanges = 0;
-  md += "## Changed Files\n";
+  let combinedChangeSize = 0;
+  md += "## Changed Files\n\n";
   let sizeChangeMd = "| file | base | pr | diff |\n";
   sizeChangeMd += "| --- | --- | --- | --- |\n";
   for (const file of changedFiles) {
@@ -95,14 +100,21 @@ async function run() {
     const computedPR = computedFile(pr, file);
     const diff = computedPR - computedBase;
     if (diff !== 0) {
+      combinedChangeSize += diff;
       fileSizeChanges += 1;
       const sign = diff >= 0 ? "+" : "-";
       sizeChangeMd += `| ${file} | ${computedBase}B | ${computedPR}B | ${sign}${diff}B |\n`;
     }
+    sizeChangeMd += "\n";
   }
 
   if (fileSizeChanges > 0) {
+    const maybeS = fileSizeChanges === 1 ? "" : "s";
+    const sign = combinedChangeSize >= 0 ? "+" : "-";
+    md += `${fileSizeChanges} file${maybeS} changed, totalling ${sign}${combinedChangeSize}B\n`;
+    md += "<details>\n";
     md += sizeChangeMd;
+    md += "</details>\n";
     md += "\n";
   } else {
     md += "No existing build file changes.";
